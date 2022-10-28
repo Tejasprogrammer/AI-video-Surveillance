cbbc93cb7bfccac561da743e06f4bec989c5d8de
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-10-28 12:41:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,14 +6,12 @@ function preload()
 {
     video=createVideo('video.mp4');
     video.hide();
-
 }
 
 function setup()
 {
     canvas = createCanvas(480, 340);
     canvas.center();
-    
 }
 
 function start()
@@ -31,7 +29,7 @@ video.speed(1);
 video.volume(0);
 }
 
-function gotresults(error, results)
+function gotResult(error, results)
 {
 if(error)
 {
@@ -44,4 +42,23 @@ objects=results;
 function draw()
 {
 image(video,0,0,480, 380);
+
+if(status !="")
+{
+objectDetector.detect(video, gotResult);
+
+for(i=0; i<objects.length; i++)
+{
+    document.getElementById("status").innerHTML="Status: Objects Detected";
+    document.getElementById("number_of_objects").innerHTML="Number of objects detected are"+objects.length;
+    fill("#f20a0a");
+    percent=floor(objects[i].confidence*100);
+    text(objects[i].label+" "+percent+"%",objects[i].x, objects[i].y);
+    nofill();
+    stroke("#f8fc0a");
+    rect(objects[i].x , objects[i].y , objects[i].width,  objects[i].height);
+}
+
+}
+
 }
